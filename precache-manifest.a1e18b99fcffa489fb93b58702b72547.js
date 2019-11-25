2d7b4b5e08e6b1dae6e5410ad153053204e150ce
Isaiah Carter <izay.carter0509@gmail.com>
2019-11-25 14:05:32 -0500

    Updates

---
+++ precache-manifest.a1e18b99fcffa489fb93b58702b72547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8864690703b85d05365085cb3ffbce9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "32a92538516b84170412",
+    "url": "/portfolio/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "713e34a9c2900f4192ee",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "32a92538516b84170412",
+    "url": "/portfolio/static/js/2.b2365ae4.chunk.js"
+  },
+  {
+    "revision": "713e34a9c2900f4192ee",
+    "url": "/portfolio/static/js/main.52391464.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  }
+]);
