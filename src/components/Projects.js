dbf6db8c59b8cd519a8b225d3077d419fd0d4142
Isaiah Carter <izay.carter0509@gmail.com>
2020-01-12 16:47:30 -0500

    added spacing to project content box

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,6 +31,18 @@ class Projects extends Component{
                         <li>Twilio API</li>
                     </ul>
                 </div>
+                <div className="d-flex project-box">
+                    <p className="project-title">Gathering Well</p>
+                    <p className="">The Gathering Well is a application  designed to help people new to the Greenville area explore christian based churches.This is a central location of information provided by the churches directly through their profile on this site. Prior to being visible to the public every profile is verified of it's context and information. This in return allows churches to present their personal characteristics as well as beliefs.</p>
+                    <ul className="d-flex tech-list">
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>React</li>
+                        <li>Django</li>
+                        <li>Google Map API</li>
+                        <li>Twilio API</li>
+                    </ul>
+                </div>
             </div>    
 
         );
